f244bf3c6b629151541fef3913f00076ac101e59
Kayzzel <kayzzel@MacBook-Air-de-Kayzzel.local>
2025-12-19 21:27:26 +0100

    now the line where I am has no bg

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -15,7 +15,7 @@ opt.shiftwidth = 4
 opt.autoindent = true
 opt.backspace = "indent,eol,start"
 
-vim.api.nvim_set_hl(0, "CursorLine", { bg = "#422d57", blend = 10 })
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
 vim.api.nvim_set_hl(0, "Visual",     { bg = "#6c4791", blend = 10 })
 vim.api.nvim_set_hl(0, "Cursor", {
   bg = "#7a7a7a",
