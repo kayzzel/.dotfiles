b8c99f1cdd2395483ec639ae6954c7e19318c79f
Gauthier Bach <gabach@z2r10p7.42lyon.fr>
2026-02-10 19:02:57 +0100

    added

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-
 # ===== COLORS ===================================== #
 
 
@@ -33,24 +32,40 @@ NC='\033[0m'
 # ===== CONFIGS ==================================== #
 
 root=(".zshrc" "startup.sh" ".tmux.conf" ".w3m")
-components=("kitty" "nvim" "macchina")
+config=("kitty" "nvim" "macchina")
 
 
 # ===== FUNCTIONS ================================== #
 
-force_symlink() {
-	read -p "Delete '$2$1' (y/N) " confirmation
+force_create_symlink() {
+	rm -rf "$2$1"
+	ln -sv "$PWD/$1" $2 2> /dev/null > /dev/null
+	if [ $? -ne 0 ]
+	then
+		printf "    ${RED}[FAIL]    ${YELLOW}CREATING${BOLD} > $2$1${NC}\n" else
+	else
+		printf "    ${GREEN}[SUCCESS] ${YELLOW}CREATING${BOLD} > $2$1${NC}\n"
+	fi
+	echo
+}
+
+ask_symlink() {
+	printf "\t      ${RED}Delete${NC} '$2$1' ${BOLD}(y/N)${NC} " 
+	read confirmation
 	confirmation="$(echo ${confirmation} | tr 'A-Z' 'a-z')"
 	if [[ $confirmation == "y" ]]
 	then
 		rm -rf "$2$1"
 		ln -sv "$PWD/$1" $2 2> /dev/null > /dev/null
+		echo
 		if [ $? -ne 0 ]
 		then
 			printf "    ${RED}[FAIL]    ${YELLOW}CREATING${BOLD} > $2$1${NC}\n"
 		else
 			printf "    ${GREEN}[SUCCESS] ${YELLOW}CREATING${BOLD} > $2$1${NC}\n"
 		fi
+	else
+			printf "    ${CYAN}[SKIPPED] ${YELLOW}NOT CREATING${BOLD} > $2$1${NC}\n"
 	fi
 }
 
@@ -60,26 +75,163 @@ create_symlink() {
 	if [ $? -ne 0 ]
 	then
 		printf "    ${RED}[FAIL]    ${YELLOW}CREATING${BOLD} > $2$1${NC}\n"
-		force_symlink $1 $2
+		ask_symlink $1 $2
 	else
 		printf "    ${GREEN}[SUCCESS] ${YELLOW}CREATING${BOLD} > $2$1${NC}\n"
 	fi
+	echo
 }
 
 
-# ===== CODE ======================================= #
-
-printf "${PURPLE}
+force_install_all() {
+	printf "${PURPLE}
 ==================================================
 \tKayzzel Setup - Symlink Dotfiles
 ==================================================
 ${NC}
 "
 
-for comp in "${components[@]}" ; do :
-	create_symlink "$comp/" "$HOME/.config/"
+	for conf in "${config[@]}" ; do :
+		force_create_symlink "$conf" "$HOME/.config/"
+	done
+
+	for conf in "${root[@]}" ; do :
+		force_create_symlink "$conf" "$HOME/"
+	done
+}
+
+
+install_all() {
+	printf "${PURPLE}
+==================================================
+\tKayzzel Setup - Symlink Dotfiles
+==================================================
+${NC}
+"
+
+	for conf in "${config[@]}" ; do :
+		create_symlink "$conf" "$HOME/.config/"
+	done
+
+	for conf in "${root[@]}" ; do :
+		create_symlink "$conf" "$HOME/"
+	done
+}
+
+list_config() {
+	echo "List config:"
+	for conf in "${config[@]}" ; do :
+		printf "   - $conf\n"
+	done
+
+	for conf in "${root[@]}" ; do :
+		printf "   - $conf\n"
+	done
+}
+
+
+is_in_array() {
+	echo "$1"
+	echo "$2"
+	for item in $2
+	do
+		if [ "$1" == "$item" ]; then
+			echo "In the list"
+			exit 0
+		fi
+	done
+	exit 0
+}
+
+
+usage()
+{
+	cat << EOF
+usage: $0 PARAM [-o|--one CONFIG] [-f|--force] [-h|--help]
+
+This script does install config files.
+
+OPTIONS:
+   PARAM        The param
+   -h|--help    Show this message
+   -l|--list	list all the configs
+   -f|--force   Enable --force
+   -o|--one 	install only one config
+EOF
+}
+
+# ===== CODE ======================================= #
+
+
+
+CONFIG=""
+FORCE=""
+
+if ! [[ "$1" ]]
+then
+	install_all
+	exit
+fi
+while [ ! $# -eq 0 ]; do
+    case "$1" in
+        -o | --one)
+			if [ "$CONFIG" ]; then
+				echo 'already choose a config'
+				exit 1
+			elif [ "$2" ]; then
+                CONFIG=$2
+                shift
+            else
+                echo '--one requires a value'
+                exit 1
+            fi
+            ;;
+        -f | --force)
+			if [ "$FORCE" ]; then
+				echo '--force already used'
+				exit 1
+			fi
+            FORCE='--force'
+            ;;
+        -h | --help)
+            usage
+            exit
+            ;;
+        -l | --list)
+			list_config
+            exit
+            ;;
+        *)
+            usage
+            exit
+            ;;
+    esac
+    shift
 done
 
-for element in "${root[@]}" ; do :
-	create_symlink "$element" "$HOME/"
-done
+
+if [ "$CONFIG" ]; then
+	if ! is_in_array $CONFIG $root; then
+		if [ "$FORCE" ]; then
+			force_create_symlink "$CONFIG" "$HOME/"
+		else
+			create_symlink "$CONFIG" "$HOME/"
+		fi
+	elif ! is_in_array $CONFIG $config; then
+		if [ "$FORCE" ]; then
+			force_create_symlink "$CONFIG" "$HOME/.config"
+		else
+			create_symlink "$CONFIG" "$HOME/.config"
+		fi
+	else
+		echo "$CONFIG is not an availaible config"
+		exit 1
+	fi
+else
+	if [ "$FORCE" ]; then
+		echo "test: $CONFIG"
+		force_install_all
+	else
+		install_all
+	fi
+fi
