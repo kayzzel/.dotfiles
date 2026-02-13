e3852a28d7262bbb62ec0e1a39aa0d9ac10a0190
Gauthier Bach <gabach@z2r12p5.42lyon.fr>
2026-02-13 18:31:50 +0100

    added noice.lua to my vim config

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,67 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+	opts = {
+		cmdline = {
+			enabled = true,
+			view = "cmdline_popup",
+		},
+
+		views = {
+			cmdline_popup = {
+				position = {
+					row = 5,      -- top of screen
+					col = "50%",  -- centered horizontally
+				},
+				size = {
+					width = 60,
+					height = "auto",
+				},
+				border = {
+					style = "rounded",
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		require("noice").setup(opts)
+
+		-- Apply highlights after colorscheme loads
+		local function set_noice_highlights()
+			-- Text you type in cmdline
+			vim.api.nvim_set_hl(0, "NoiceCmdline", {
+				fg = "#ff79c6", -- change to whatever color you like
+				bg = "NONE",    -- transparent
+			})
+
+			-- Popup background transparent
+			vim.api.nvim_set_hl(0, "NoiceCmdlinePopup", {
+				bg = "NONE",
+			})
+
+			-- Popup border (optional styling)
+			vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorder", {
+				fg = "#89b4fa",
+				bg = "NONE",
+			})
+
+			-- Prompt text color
+			vim.api.nvim_set_hl(0, "NoiceCmdlinePrompt", {
+				fg = "#a6e3a1",
+				bg = "NONE",
+			})
+		end
+
+		-- Run immediately
+		set_noice_highlights()
+
+		-- Reapply after colorscheme changes
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			callback = set_noice_highlights,
+		})
+	end,
+}
