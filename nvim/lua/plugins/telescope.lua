2e0979b20cd6ecec2112016a902b049109dcc94f
Gauthier Bach <gabach@z2r10p6.42lyon.fr>
2026-02-18 18:48:36 +0100

    changed telescope beavior so that I can go up and down with <C-j> and <C-k> and it closes with escape

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,15 +1,63 @@
 return {
-	{
-		'nvim-telescope/telescope.nvim',
-		tag = 'v0.2.0',
-		dependencies = {
-			'nvim-lua/plenary.nvim',
-			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
-		},
-		config = function()
-			require('telescope').setup {
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = 'v0.2.0',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+      local actions = require("telescope.actions")
 
-			}
-		end
-	}
+      require("telescope").setup({
+        defaults = {
+          prompt_prefix = "🔍 ",
+          selection_caret = "➤ ",
+          entry_prefix = "  ",
+          initial_mode = "insert",
+          selection_strategy = "reset",
+          sorting_strategy = "ascending",
+          layout_strategy = "horizontal",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              preview_width = 0.55,
+              results_width = 0.8,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+          },
+          file_ignore_patterns = { "node_modules", "%.git/" },
+          path_display = { "truncate" },
+
+          mappings = {
+            i = {
+              -- Close Telescope immediately when pressing <Esc>
+              ["<Esc>"] = actions.close,
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+              ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+            },
+            n = {
+              ["q"] = actions.close,
+              ["j"] = actions.move_selection_next,
+              ["k"] = actions.move_selection_previous,
+            },
+          },
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          }
+        }
+      })
+
+      -- Load fzf extension
+      require('telescope').load_extension('fzf')
+    end
+  }
 }
